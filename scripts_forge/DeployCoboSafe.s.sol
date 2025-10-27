99c372bc40fd2204d40d51e3ab03647a5c49f532
Sean <877035725@qq.com>
2025-10-27 16:51:43 +0800

    feat: deploy cofosafe

--- scripts_forge/DeployCoboSafe.s.sol
+++ scripts_forge/DeployCoboSafe.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {Script, console2 as console} from "forge-std/Script.sol";
 import {CoboSafeFactory} from "../contracts/helper/CoboSafeFactory.sol";
 import {CoboFactory} from "../contracts/CoboFactory.sol";
+import {IGnosisSafe} from "../contracts/helper/CoboSafeFactory.sol";
 
 contract DeployCoboScript is Script {
     CoboSafeFactory public factory;
@@ -11,15 +12,42 @@ contract DeployCoboScript is Script {
     function run() external {
         factory = new CoboSafeFactory();
         console.log("coboArgusFactory:", factory.coboArgusFactory());
-        console.log("gnosisSafeProxyFactory:", factory.gnosisSafeProxyFactory());
-        console.log("gnosisSafeFallbackHandler:", factory.gnosisSafeFallbackHandler());
 
-        // address coboFactoryAddr = factory.coboArgusFactory();
-        // console.log("coboFactoryAddr:", coboFactoryAddr);
-        // CoboFactory coboFactory = CoboFactory(coboFactoryAddr);
-        // address helper_addr = coboFactory.getLatestImplementation(bytes32("ArgusAccountHelper"));
-        // require(helper_addr != address(0), "ArgusAccountHelper implementation not found");
+        address coboFactoryAddr = factory.coboArgusFactory();
+        CoboFactory coboFactory = CoboFactory(coboFactoryAddr);
+        address helper_addr = coboFactory.getLatestImplementation(
+            bytes32("ArgusAccountHelper")
+        );
+        console.log("helper_impl:", helper_addr);
+        bytes memory data = abi.encodeWithSignature(
+            "initArgus(address,bytes32)",
+            address(coboFactoryAddr),
+            bytes32(0x0)
+        );
+        console.logBytes(data);
 
-        // console.log("helper_impl:", helper_addr);
+        address safe_addr = vm.envAddress("SAFE_ADDRESS");
+        uint256 pk = vm.envUint("PRIVATE_KEY");
+        address addr = vm.addr(pk);
+
+        bytes memory signature = abi.encodePacked(
+            abi.encode(addr, addr),
+            bytes1(0x01)
+        );
+        vm.startBroadcast(pk);
+        IGnosisSafe(safe_addr).execTransaction(
+            helper_addr,
+            0,
+            data,
+            uint8(1),
+            0,
+            0,
+            0,
+            address(0),
+            payable(0),
+            signature
+        );
+        vm.stopBroadcast();
+        console.log("Transaction successfully sent!");
     }
 }
